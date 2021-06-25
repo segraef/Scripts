26e3a06fc40c5a4857bf1394ffbbb140fd941e1b
Sebastian <sebastian@graef.io>
2021-06-25 11:20:10 +0800

    init (#1)
    * init
    
    * linted
    
    * linted
    
    * bash
    
    * linted
    
    * scripts
    
    * badges
    
    * linter

---
+++ PowerShell/Export-AzDOBuildReleaseDefinitions.ps1
@@ -0,0 +1,149 @@
+﻿<#
+    .SYNOPSIS
+      Exports build and release definitions from a source Azure DevOps project and imports these into a destination project.
+      If no destination project give it will only export build and release defintions and save it as JSON.
+
+    .DESCRIPTION
+      The script will create two folders with all exported build and release definitions as JSON.
+      Once a destination organization and project are given, the script will import the exported
+      build and release defintions into the target project.
+
+      Required Modules (will be installed if not present): VSTeam
+
+    .PARAMETER sourceAccount
+        Azure DevOps source account/organization.
+
+    .PARAMETER sourceProject
+        Azure DevOps source project.
+
+    .PARAMETER sourcePersonalAccessToken
+        Azure DevOps source Personal Access Token.
+
+    .PARAMETER sourcePersonalAccessToken
+        Azure DevOps source Secure Access Token.
+
+    .PARAMETER destinationAccount
+        Azure DevOps destination account/organization.
+
+    .PARAMETER destinationProject
+        Azure DevOps destination account/organization.
+
+    .PARAMETER destinationPersonalAccessToken
+        Azure DevOps destination Personal Access Token.
+
+    .PARAMETER destinationSecureAccessToken
+        Azure DevOps destination Secure Access Token.
+
+    .NOTES
+      Version:        1.0
+      Author:         Sebastian Gräf
+      Email:          sebastian@graef.io
+      Creation Date:  08/13/2019
+      Purpose/Change: Initial script development
+#>
+
+#region Parameters
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$sourceAccount,
+    [Parameter(Mandatory = $true)]
+    [string]$sourceProject,
+    [Parameter(Mandatory = $true)]
+    [string]$sourcePersonalAccessToken,
+    [Parameter(Mandatory = $false)]
+    [securestring]$sourceSecureAccessToken,
+    [Parameter(Mandatory = $False)]
+    [string]$destinationAccount,
+    [Parameter(Mandatory = $False)]
+    [string]$destinationProject,
+    [Parameter(Mandatory = $False)]
+    [string]$destinationPersonalAccessToken,
+    [Parameter(Mandatory = $False)]
+    [securestring]$destinationSecureAccessToken
+)
+
+#endregion
+
+#region Initialisations
+
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "Continue"
+
+Import-Module ..\Load-Module.ps1
+
+#endregion
+
+#region Declarations
+#endregion
+
+#region Functions
+#endregion
+
+#region Execution
+
+Load-Module VSTeam
+
+if ($sourceAccount -and $sourceProject) {
+    # Set the source project
+    if ($sourcePersonalAccessToken) {
+        Set-VSTeamAccount -Account $sourceAccount -PersonalAccessToken $sourcePersonalAccessToken
+    }
+    elseif ($sourcePersonalAccessToken) {
+        Set-VSTeamAccount -Account $sourceAccount -SecurePersonalAccessToken $sourceSecureAccessToken
+    }
+    else {
+        Write-Output "Exiting script since no token given"
+    }
+
+    # Get all release definitions
+    $releaseDefinitions = Get-VSTeamReleaseDefinition -ProjectName $sourceProject
+
+    # Get all build definitions
+    $buildDefinitions = Get-VSTeamBuildDefinition -ProjectName $sourceProject
+
+    # Create definition folders
+    $buildDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.BuildDefinitions" -ItemType Directory -Force
+    $releaseDefinitionDirectory = New-Item "$sourceAccount.$sourceProject.ReleaseDefinitions" -ItemType Directory -Force
+
+    # Export build defintions
+    foreach ($buildDefinition in $buildDefinitions) {
+        $fileName = $buildDefinitionDirectory.FullName + "\" + $buildDefinition.Name + ".json"
+        Get-VSTeamBuildDefinition -ProjectName $sourceProject -Id $buildDefinition.ID -json | Out-File $fileName
+    }
+    Write-Output "Your build definitions can be found here: $buildDefinitionDirectory"
+
+    # Export release defintions
+    foreach ($releaseDefinition in $releaseDefinitions) {
+        $fileName = $releaseDefinitionDirectory.FullName + "\" + $releaseDefinition.Name + ".json"
+        Get-VSTeamReleaseDefinition -ProjectName $sourceProject -Id $releaseDefinition.ID -json | Out-File $fileName
+    }
+    Write-Output "Your release definitions can be found here: $releaseDefinitionDirectory"
+}
+
+if ($destinationAccount -and $destinationProject) {
+    # Set the destination project
+    if ($destinationPersonalAccessToken) {
+        Set-VSTeamAccount -Account $destinationAccount -PersonalAccessToken $destinationPersonalAccessToken
+    }
+    elseif ($destinationSecureAccessToken) {
+        Set-VSTeamAccount -Account $sourceAccount -SecurePersonalAccessToken $destinationSecureAccessToken
+    }
+    else {
+        Write-Output "Exiting script since no token given"
+    }
+
+    # Import release defintions
+    $releaseDefinitions = Get-ChildItem $releaseDefinitionDirectory.FullName
+    foreach ($releaseDefinition in $releaseDefinitions) {
+        $fileName = $releaseDefinition.FullName
+        Add-VSTeamReleaseDefinition -ProjectName $destinationProject -inFile $fileName
+    }
+
+    # Import build defintions
+    $buildDefinitions = Get-ChildItem $buildDefinitionDirectory.FullName
+    foreach ($buildDefinition in $buildDefinitions) {
+        $fileName = $buildDefinition.FullName
+        Add-VSTeamReleaseDefinition -ProjectName $destinationProject -inFile $fileName
+    }
+}
---
+++ PowerShell/Get-DevOpsPrivateRepoFile.ps1
@@ -0,0 +1,111 @@
+#Requires -Version 5.1
+
+<#
+.SYNOPSIS
+  <Overview of script>
+
+.DESCRIPTION
+  <Brief description of script>
+
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+
+.INPUTS
+  <Inputs if any, otherwise state None>
+
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#region Parameters
+
+[CmdletBinding()]
+param
+(
+  [Parameter()]
+  [String]$devOpsAccountName = 'segraef',
+  [Parameter()]
+  [String]$devOpsTeamProjectName = 'Oahu',
+  [Parameter()]
+  [String]$devOpsPAT = 'xxx',
+  [Parameter()]
+  [String]$devOpsBaseUrl = 'https://' + $devOpsAccountName + '.visualstudio.com',
+  [Parameter()]
+  [String]$FileRepo = 'Oahu',
+  [Parameter()]
+  [String]$FileRepoBranch = 'master',
+  [Parameter()]
+  [String]$FilePath = 'Scripts/PowerShell/123.ps1',
+  [Parameter()]
+  [String]$User = ''
+)
+
+#endregion
+
+#region Initialisations
+
+$ErrorActionPreference = "Continue"
+$VerbosePreference = "Continue"
+
+# Dot Source required Function Libraries
+Import-Module ..\Write-Log.ps1
+
+#endregion
+
+#region Declarations
+#endregion
+
+#region Functions
+
+function FunctionName {
+  Param()
+
+  begin {
+    Write-Log "Let's start !"
+  }
+
+  process {
+    try {
+      Write-Output "Hello Template !"
+    }
+
+    catch {
+      Write-Output $_
+      Write-Log $_ -Warning
+    }
+  }
+
+  end {
+    if ($?) {
+      Write-Log "Completed successfully !"
+    }
+  }
+}
+
+#endregion
+
+#region Execution
+
+Write-Log "Executing $($MyInvocation.MyCommand.Name)"
+
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $User,$devOpsPAT)));
+$devOpsAuthHeader = @{Authorization=("Basic {0}" -f $base64AuthInfo)};
+
+$Uri = $devOpsBaseUrl + '/' + $devOpsTeamProjectName + '/_apis/git/repositories/' + $FileRepo  + '/items?path=' + $FilePath + '&$format=json&includeContent=true&versionDescriptor.version=' + $FileRepoBranch + '&versionDescriptor.versionType=branch&api-version=4.1'
+
+$File = Invoke-RestMethod -Method Get -ContentType application/json -Uri $Uri -Headers $devOpsAuthHeader
+
+Write-Output $File.content
+
+Write-Log "Finished executing $($MyInvocation.MyCommand.Name)"
+
+#endregion
