ac9e4e6febc39f58a6ce2e3b7e5892ee28b3b25e
Sebastian Graef <segraef@microsoft.com>
2022-07-29 14:08:48 +1000

    b

--- PowerShell/Snippets/Rename-Items.ps1
+++ PowerShell/Snippets/Rename-Items.ps1
@@ -1,5 +1,5 @@
 # With -Directory for folders and without for files
-$searchprefix = 'nawtasp*'
-$a = 'nawtasp'
+$searchprefix = 'bla*'
+$a = 'bla'
 $b = 'prfx'
 Get-ChildItem -Path $searchprefix -Recurse | ForEach-Object -Process { Rename-item -Path $_.FullName -NewName ($_.name -replace $a, $b) -Verbose -whatif}
