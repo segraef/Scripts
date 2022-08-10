ce5d2f71e514d20aeaaf2c34e60836335b7069ec
Sebastian Graef <segraef@microsoft.com>
2022-08-10 15:24:11 +1000

    whatif

--- PowerShell/Snippets/Rename-Items.ps1
+++ PowerShell/Snippets/Rename-Items.ps1
@@ -1,5 +1,7 @@
 # With -Directory for folders and without for files
-$searchprefix = 'bla*'
-$a = 'bla'
-$b = 'prfx'
-Get-ChildItem -Path $searchprefix -Recurse | ForEach-Object -Process { Rename-item -Path $_.FullName -NewName ($_.name -replace $a, $b) -Verbose -whatif}
+$WhatIfPreference = 'False'
+
+$searchprefix = '*eus*'
+$a = 'eus'
+$b = 'ae'
+Get-ChildItem -Path $searchprefix -Recurse | ForEach-Object -Process { Rename-item -Path $_.FullName -NewName ($_.name -replace $a, $b) -Verbose}
