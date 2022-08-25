227253508d27b21047526f9f31bec9341589d465
Sebastian Graef <segraef@microsoft.com>
2022-08-26 09:47:08 +1000

    -SkipCertificateCheck

--- _in progress/prefix-dev-ae-devops-rg/scripts/script.ps1
+++ _in progress/prefix-dev-ae-devops-rg/scripts/script.ps1
@@ -41,7 +41,7 @@ if (-not (Test-Path "$PWD\agent\")) {
     Write-Host "1. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
 
     $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
-    $package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
+    $package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1" -SkipCertificateCheck
     $packageUrl = $package[0].Value.downloadUrl
 
     Write-Host $packageUrl
