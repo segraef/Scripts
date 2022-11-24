66cd44768909692c74a0f40768ecc293cbb00594
Sebastian Graef <segraef@microsoft.com>
2022-11-24 19:34:20 +1000

    .

--- PowerShell/Snippets/Cleanup-LocalGitBranches.ps1
+++ PowerShell/Snippets/Cleanup-LocalGitBranches.ps1
@@ -1,8 +1,10 @@
-$folder = '.'
-$localRepos = Get-ChildItem $folder -Directory | select *
+$folder = Get-Location # '.'
+$localRepos = Get-ChildItem $folder.Path -Directory | select *
 
 foreach($repo in $localRepos) {
     "$($repo.FullName)"
-    cd "$($repo.FullName)"
+    Set-Location "$($repo.FullName)"
+    git tag -d 1.0.0
     git checkout main; git pull; git remote update origin --prune; git branch -vv | Select-String -Pattern ": gone]" | % { $_.toString().Trim().Split(" ")[0]} | % { git branch -D $_ }
- }
+}
+Set-Location $folder.Path
