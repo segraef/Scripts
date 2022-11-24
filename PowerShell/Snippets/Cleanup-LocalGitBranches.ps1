4e93e623b471e91da98d9cd7cbef76bc26c02c14
Sebastian Graef <segraef@microsoft.com>
2022-11-24 19:23:44 +1000

    .

--- PowerShell/Snippets/Cleanup-LocalGitBranches.ps1
+++ PowerShell/Snippets/Cleanup-LocalGitBranches.ps1
@@ -1,7 +1,8 @@
 $folder = '.'
-$localRepos = Get-ChildItem $folder -Recurse
+$localRepos = Get-ChildItem $folder -Directory | select *
 
 foreach($repo in $localRepos) {
-    Set-Locatation $repo.FullName
-    # git checkout main; git pull; git remote update origin --prune; git branch -vv | Select-String -Pattern ": gone]" | % { $_.toString().Trim().Split(" ")[0]} | % { git branch -D $_ }
+    "$($repo.FullName)"
+    cd "$($repo.FullName)"
+    git checkout main; git pull; git remote update origin --prune; git branch -vv | Select-String -Pattern ": gone]" | % { $_.toString().Trim().Split(" ")[0]} | % { git branch -D $_ }
  }
