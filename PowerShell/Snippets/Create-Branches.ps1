8911fe349a349d2724e14e8386f56b164ac78a87
Sebastian Graef <segraef@microsoft.com>
2022-12-07 15:00:51 +1000

    .

--- PowerShell/Snippets/Create-Branches.ps1
+++ PowerShell/Snippets/Create-Branches.ps1
@@ -7,6 +7,10 @@ $repos = Get-ChildItem -Path $destinationFolder -Directory
 foreach($repo in $repos) {
    $($repo.FullName)
    Set-Location "$($repo.FullName)";
-   git checkout main;
-   git pull;
+   git checkout -b users/segraef/provider-upgrade;
+   git push --set-upstream origin users/segraef/provider-upgrade;
+   git add .;
+   git commit -m "provider upgrade";
+   git push;
 }
+
