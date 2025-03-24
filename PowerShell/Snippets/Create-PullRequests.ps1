06acff3b23c2be7043fa74c12e8537a5026daba0
Az-Tech Platform <az-techH@team.telstra.com>
2025-03-24 19:37:13 +1000

    Refactor Create-PullRequests script to use CmdletBinding and support ShouldProcess for safer execution

--- PowerShell/Snippets/Create-PullRequests.ps1
+++ PowerShell/Snippets/Create-PullRequests.ps1
@@ -1,19 +1,26 @@
-Param(
-   [string]$org = "x00",
-   [string]$project = "Modules",
-   [string]$token = "token",
-   [string]$branchName = "users/segraef/provider-upgrade"
-)
+function Create-PullRequests {
 
-az extension add --name azure-devops
+    [CmdletBinding(SupportsShouldProcess)]
+    Param(
+        [Parameter()]
+        [string]$org = "x00",
+        [Parameter()]
+        [string]$project = "Modules",
+        [Parameter()]
+        [string]$token = "token",
+        [Parameter()]
+        [string]$branchName = "users/segraef/provider-upgrade"
+    )
 
-Write-Output $token | az devops login --organization https://dev.azure.com/$org
-az devops configure --defaults organization=https://dev.azure.com/$org project=$project
-$repos = az repos list | ConvertFrom-Json
+    az extension add --name azure-devops
 
-# Create Pull Request on all repos
-foreach($repo in $repos){
-    Write-Output $repo.name
-    az repos pr create --repository $repo.name --source-branch $branchName --open --output table
+    Write-Output $token | az devops login --organization https://dev.azure.com/$org
+    az devops configure --defaults organization=https://dev.azure.com/$org project=$project
+    $repos = az repos list | ConvertFrom-Json
+
+    # Create Pull Request on all repos
+    foreach ($repo in $repos) {
+        Write-Output $repo.name
+        az repos pr create --repository $repo.name --source-branch $branchName --open --output table
+    }
 }
-
