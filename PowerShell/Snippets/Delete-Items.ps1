7275f3dc582603205cef11632fb13c44cb55f1d8
Sebastian Graef <sebastian@graef.io>
2026-06-08 17:36:03 +1000

    Remove obsolete Azure DevOps pipeline configurations and scripts
    - Deleted pipeline.variables.yml and pipeline.yml as they are no longer needed.
    - Removed parameters.json files for private endpoints, storage accounts, and virtual machines.
    - Eliminated scripts for installing pipeline agents and runtime configurations for bare metal and RHEL.
    - Cleaned up unnecessary PowerShell and shell scripts related to Azure DevOps agent setup.
    - Removed the process-aiml-resource-groups.ps1 script for managing Azure resource groups.

--- PowerShell/Snippets/Delete-Items.ps1
+++
@@ -1,6 +0,0 @@
-# With -Directory for folders and without for files
-$WhatIfPreference = 'False'
-
-# $searchprefix = '.git'
-$searchprefix = '.pre*'
-Get-ChildItem -Path $searchprefix -Recurse -Hidden | ForEach-Object -Process { Remove-Item -Path $_.FullName -Verbose -Recurse -Force}
